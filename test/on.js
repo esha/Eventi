e5c547741f7b05c20eb3c6cd4b091e42591b2773
Nathan Bubna <nathan@esha.com>
2014-02-05 15:48:26 -0800

    more tests for on internals

--- test/on.js
+++ test/on.js
@@ -35,6 +35,34 @@
     equal(Eventi.fy({}).on, Eventi.on, 'should get on()');
   });
 
+  test('_.listener', function() {
+    var o = {},
+      listener = _.listener(o);
+    equal(typeof listener, "function", 'should get listener function back');
+    equal(typeof listener.s, "object", "should have 's' object");
+    equal(_.listener(o), listener, 'should always get same listener back');
+    for (var key in o) {
+      ok(false, 'object should not have key: '+key);
+    }
+
+  });
+
+  test('_.closest', function() {
+    equal(_.closest(document, 'foo'), undefined, 'undefined for non Elements');
+    var root = document.querySelector('#test_on'),
+        div = root.querySelector('.closest');
+    equal(_.closest(div, 'div'), div, 'el should prefer itself to parent');
+    equal(_.closest(div, '#test_on'), root, 'should find parent when element does not match');
+  });
+
+  test('_.matches', function() {
+    var e = new Eventi('cat:type#tag(detail)');
+    ok(_.matches(e, {}), 'should always match empty object');
+    ok(!_.matches(e, {foo:'bar'}), 'should not match random object');
+    ok(_.matches(e, {type:'type'}), 'should match type alone');
+    ok(_.matches(e, {category:'cat',type:'type',tag:true,detail:'detail'}), 'should match everything');
+  });
+
   test('internal api presence', function() {
     ok(_.on, "_.on");
     ok(_.handler, "_.handler");
