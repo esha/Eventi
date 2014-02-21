39be4bb1c021218d47a5debc527d8f88632b7566
nbubna <nathan@esha.com>
2014-02-21 11:03:12 -0800

    finish whipping singleton into shape

--- src/singleton.js
+++ src/singleton.js
@@ -4,56 +4,46 @@ _.properties.unshift([/^\^/, function singleton(){ this.singleton = true; }]);
 // _.fire's _.dispatch will call this when appropriate
 _.singleton = function(target, event) {
 	_.remember(target, event);
-	if (event.bubbles && target !== _.global) {
+	if (event.bubbles && !event.propagationStopped && target !== _.global) {
 		_.singleton(target.parentNode || target.parentObject || _.global, event);
 	}
 };
 var _skey = _._skey = '^'+_key;
 _.remember = function remember(target, event) {
-	global.console.log(event.type, 'remember', target, event.timeStamp);
-	if (!target[_skey]) {
-		var saved = target[_skey];
-		event[_skey] = true;
-		if (saved) {
-			saved.push(event);
-		} else {
-			Object.defineProperty(target, _skey, {value:[event],configurable:true});
-		}
+	var saved = target[_skey] || [];
+	if (!saved.length) {
+		Object.defineProperty(target, _skey, {value:saved,configurable:true});
 	}
+	event[_skey] = true;
+	saved.push(event);
 };
 
 Eventi.on(_, 'handler#new', function singletonHandler(e, handler) {
 	if (handler.match.singleton) {
 		delete handler.match.singleton;// singleton never needs matching
-		var fn = handler.fn,
+		var fn = handler._fn = handler.fn,
 			target = handler.target;
 		handler.fn = function single(e) {
-			if (_.off){ _.off(target, [handler.text], fn); }
-			handler.fn = _.noop;
-			_.remember(target, e);
+			_.unhandle(handler);
+			if (!e[_skey]) {// remember this non-singleton as singleton for handler's sake
+				_.remember(target, e);
+			}
 			fn.apply(this, arguments);
 		};
-global.console.log(handler.match.type, 'handler', target, target[_skey]);
+
 		// search target's saved singletons, execute handler upon match
-		var saved = target[_skey];
-		if (saved) {
-global.console.log('saved', target, saved.length);
-			for (var i=0,m=saved.length,stop; i<m && !stop; i++) {
-				stop = _.remembered(target, saved[i], handler);
-if (stop){ global.console.log(handler.match.type, 'remembered', target); }
+		var saved = target[_skey]||[];
+		for (var i=0,m=saved.length; i<m; i++) {
+			var event = saved[i];
+			if (_.matches(event, handler.match)) {
+				var etarget = _.target(handler, event.target);
+				if (etarget) {
+					return _.execute(etarget, event, handler);
+				}
 			}
 		}
 	}
 });
-_.remembered = function(target, event, handler) {
-	if (_.matches(event, handler.match)) {
-		if (target = _.target(handler, event.target)) {
-			return _.async(function() {
-				_.execute(target, event, handler);
-			});
-		}
-	}
-};
 
 if (document) {
 	Eventi.on('DOMContentLoaded', function ready(e) {
--- test/singleton.js
+++ test/singleton.js
@@ -23,10 +23,98 @@
   var _ = Eventi._;
   module('Eventi singleton');
 
+  test('^ready - even when registered late', function() {
+    expect(3);
+    Eventi.on('^ready', function(e, oe) {
+      equal(e.type, 'ready');
+      ok(e.singleton);
+      equal(oe.type, 'DOMContentLoaded');
+    });
+  });
+
+  test('^once on once', function() {
+    expect(5);
+    var target = Eventi.fy({});
+    target.on('^once', function(e) {// early listener
+      equal(e.type, 'once');
+      ok(!e.singleton);
+      equal(e[_._skey], true);
+    });
+
+    var first = target.fire('once#first');
+    target.fire('once#second');
+    target.on('^once', function(e) {// late listener
+      strictEqual(e, first, 'should get first once in late listener');
+      first = null;
+    });
+    ok(!first, 'late listener should have fired synchronously');
+    target.fire('once#third');
+  });
+
+  test('^sbubbles - parentNode and parentObject', function() {
+    expect(2);
+    var target = Eventi.fy({parentObject:{}});
+    target.fire('^sbubbles');
+    Eventi.on(target.parentObject, '^sbubbles', function(e) {
+      equal(e.type, 'sbubbles');
+    });
+
+    target = document.getElementById('on');
+    Eventi.fire(target, '^sbubbles');
+    Eventi.on(document.body, '^sbubbles', function(e) {
+      equal(e.type, 'sbubbles');
+    });
+  });
+
+  test('snobubble', function() {
+    expect(2);
+    var target = document.getElementById('on');
+    ok(target, 'should have target element');
+    Eventi.on(target, '^snobubble', function(e) {
+      equal(e.type, 'snobubble');
+    });
+    Eventi.fire(target, '^_snobubble');
+    Eventi.on('^snobubble', function() {
+      ok(false, 'should not see snobubble here');
+    });
+  });
+
   test('internal api presence', function() {
     var e = {};
     _.properties[0][1].call(e);
     ok(e.singleton, '_.properties has singleton parser up front');
+    ok(_.singleton, '_.singleton');
+    ok(_._skey, '_._key');
+    ok(_.remember, '_.remember');
+  });
+
+  test('_.singleton bubbles', function() {
+    var e = {type:'singletonbubbles',bubbles:true};
+    _.singleton(document.body, e);
+    equal(document.body[_._skey].pop(), e, 'body should remember e');
+    equal(document[_._skey].pop(), e, 'document should remember e');
+    equal(_.global[_._skey].pop(), e, 'global should remember e');
+  });
+
+  test('_.singleton bubbles=false', function() {
+    var e = {type:'singleton'};
+    _.singleton(document.body, e);
+    equal(document.body[_._skey].pop(), e, 'body should remember e');
+    notEqual((document[_._skey]||[]).slice(-1), e, 'document should not remember e');
+    notEqual((_.global[_._skey]||[]).slice(-1), e, 'global should not remember e');
+  });
+
+  test('_.remember', function() {
+    var target = {},
+      event = {};
+    equal(Object.keys(target).length, 0, 'target should have no keys');
+    _.remember(target, event);
+    equal(Object.keys(target).length, 0, 'target should still have no keys');
+    var saved = target[_._skey];
+    ok(saved, 'target should have saved events array');
+    equal(saved[0], event, 'should have event saved');
+    _.remember(target, event);
+    equal(saved.length, 2, 'should have two saved');
   });
 
 }());
