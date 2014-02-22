bc5983f4623bd06539626e679c9fc5055c925723
nbubna <nathan@esha.com>
2014-02-21 16:13:20 -0800

    tabs->spaces

--- src/off.js
+++ src/off.js
@@ -1,46 +1,49 @@
 _.off = function(target, events, fn) {
-	var listener = target[_key];
-	if (listener) {
-		for (var i=0, m=events.length; i<m; i++) {
-			var filter = { fn:fn },
-				type = _.parse(events[i], filter.match = {});
-			if (type) {
-				_.clean(type, filter, listener, target);
-			} else {
-				for (type in listener.s) {
-					_.clean(type, filter, listener, target);
-				}
-			}
-		}
-		if (_.empty(listener.s)) {
-			delete target[_key];
-		}
-	}
+    var listener = target[_key];
+    if (listener) {
+        for (var i=0, m=events.length; i<m; i++) {
+            var filter = { fn:fn },
+            type = _.parse(events[i], filter.match = {});
+            if (type) {
+                _.clean(type, filter, listener, target);
+            } else {
+                for (type in listener.s) {
+                    _.clean(type, filter, listener, target);
+                }
+            }
+        }
+        if (_.empty(listener.s)) {
+            delete target[_key];
+        }
+    }
 };
-_.unhandle = function(handler) {
-	_.off(handler.target, [handler.text], handler._fn||handler.fn);
-	handler.fn = _.noop;//TODO: remove this once we have confidence in _.off
+_.unhandle = function off(handler) {
+    _.off(handler.target, [handler.text], handler._fn||handler.fn);
+    handler.fn = _.noop;//TODO: remove this once we have confidence in _.off
 };
 _.empty = function(o){ for (var k in o){ return !k; } return true; };
 _.clean = function(type, filter, listener, target) {
-	var handlers = listener.s[type];
-	if (handlers) {
-		for (var i=0, m=handlers.length; i<m; i++) {
-			if (_.cleans(handlers[i], filter)) {
-				Eventi.fire(_, 'handler#off', handlers.splice(i--, 1)[0]);
-				m--;
-			}
-		}
-		if (!handlers.length) {
-			if (target.removeEventListener) {
-				target.removeEventListener(type, listener);
-			}
-			delete listener.s[type];
-		}
-	}
+    var handlers = listener.s[type];
+    if (handlers) {
+        for (var i=0, m=handlers.length; i<m; i++) {
+            if (_.cleans(handlers[i], filter)) {
+                var cleaned = handlers.splice(i--, 1)[0];
+                if (target !== _) {// ignore internal events
+                    Eventi.fire(_, 'handler#off', cleaned);
+                }
+                m--;
+            }
+        }
+        if (!handlers.length) {
+            if (target.removeEventListener) {
+                target.removeEventListener(type, listener);
+            }
+            delete listener.s[type];
+        }
+    }
 };
 _.cleans = function(handler, filter) {
-	return _.matches(handler.match, filter.match) && (!filter.fn || handler.fn === filter.fn);
+return _.matches(handler.match, filter.match) &&
+       (!filter.fn || filter.fn === (handler._fn||handler.fn));
 };
-
 Eventi.off = _.wrap('off', 3);
