e603a625ab7f2e12c85d7a706a4952061f05203e
nbubna <nathan@esha.com>
2014-04-03 13:53:32 -0700

    finish end.js tests

--- test/end.js
+++ test/end.js
@@ -3,6 +3,57 @@
     var _ = Eventi._;
     module('Eventi end');
 
+    test('Eventi.on("count$2", fn)', function() {
+        expect(2);
+        Eventi.on('count$2', function(e) {
+            equal(e.type, 'count');
+        });
+        Eventi.fire('count count count');
+    });
+
+    test('Eventi.on("type$end",fn)', function() {
+        expect(2);
+        window.end = function(e) {
+            equal(e.type, 'type');
+            delete window.end;
+            return true;
+        };
+        Eventi.on("type$end", function(e) {
+            ok(e, 'once only');
+        });
+        Eventi.fire('type type');
+    });
+
+    test('Eventi.on("not$!nested.fn",fn)', function() {
+        expect(3);
+        window.nested = {
+            fn: function(e) {
+                equal(e.type, 'not');
+                strictEqual(this, window);
+                delete window.nested;
+                return false;
+            }
+        };
+        Eventi.on("not$!nested", function(e) {
+            ok(e, 'once only');
+        });
+        Eventi.fire('not not not');
+    });
+
+    test('Eventi.on(obj, local$ender, fn)', function() {
+        expect(4);
+        var target = {
+            ender: function(e) {
+                equal(this, target);
+                return e.last;
+            }
+        };
+        Eventi.on(target, 'local$ender', function(e) {
+            equal(e.type, 'local');
+        });
+        Eventi.fire(target, 'local local#last local');
+    });
+
     test('internal api presence', function() {
         ok(_.endTest, "_.endTest");
     });
