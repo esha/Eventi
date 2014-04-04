dea8d13370bd480a68315f0f2f7f99dd5d82531b
nbubna <nathan@esha.com>
2014-04-03 23:26:13 -0700

    tests and updates for key.js

--- src/key.js
+++ src/key.js
@@ -1,20 +1,19 @@
-_.parsers.push([/\[([a-z-0-9,\.\/\[\`\\\]\']+)\]/, function(event, handler, name) {//'
-    var dash, key;
-    while ((dash = name.indexOf('-')) > 0) {
-        key = name.substring(0, dash);
-        name = name.substring(dash+1);
-        event[(_.special[key]||key)+'Key'] = true;
+_.split.guard['['] = ']';
+_.parsers.push([/\[([^ ]+)\]/, function(event, handler, key) {//'
+    var dash;
+    while ((dash = key.indexOf('-')) > 0) {
+        event[key.substring(0, dash)+'Key'] = true;
+        key = key.substring(dash+1);
     }
-    if (name) {
-        event.keyCode = _.codes[name] || parseInt(name, 10) || name;
+    if (key) {
+        event.keyCode = _.codes[key] || parseInt(key, 10) || key;
     }
 }]);
-_.special = { command: 'meta', apple: 'meta' };
 _.codes = {
     backspace:8, tab:9, enter:13, shift:16, ctrl:17, alt:18, capsLock:20, escape:27, start:91, command:224,
     pageUp:33, pageDown:34, end:35, home:36, left:37, up:38, right:39, down:40, insert:45, 'delete':46,
     multiply:106, plus:107, minus:109, point:110, divide:111, numLock:144,// numpad controls
-    ',':188, '.':190, '/':191, '`':192, '[':219, '\\':220, ']':221, '\'':222, space:32// symbols
+    ';':186, '=':187, ',':188, '-':189, '.':190, '/':191, '`':192, '[':219, '\\':220, ']':221, '\'':222, space:32// symbols
 };
 for (var n=0; n<10; n++){ _.codes['num'+n] = 96+n; }// numpad numbers
 for (var f=1; f<13; f++){ _.codes['f'+f] = 111+f; }// function keys
--- test/key.js
+++ test/key.js
@@ -20,11 +20,46 @@
       throws(block, [expected], [message])
   */
 
-  //var _ = Eventi._;
+  var _ = Eventi._;
   module('Eventi key');
 
+  test('Eventi.on("fake[ctrl-meta-shift-a]",fn)', function() {
+    Eventi.on('key[ctrl-meta-shift-a]', function(e) {
+      equal(e.type, 'key');
+      equal(e.ctrlKey, true);
+      equal(e.metaKey, true);
+      equal(e.shiftKey, true);
+      equal(e.keyCode, 65);
+    });
+    Eventi.fire('key[meta-shift-ctrl-a]');
+  });
+
+  test('Eventi.on("nocode[ctrl-]", fn)', function() {
+    Eventi.on("nocode[ctrl-]", function(e) {
+      equal(e.ctrlKey, true);
+      ok(!e.keyCode);
+    });
+    Eventi.fire('nocode[ctrl-]');
+  });
+
+  test('Eventi.on("dash[shift--] dash[-]",fn)', function() {
+    expect(2);
+    Eventi.on("dash[shift--] dash[-]", function(e) {
+      equal(e.keyCode, 189);
+    });
+    Eventi.fire('dash[shift--]');
+  });
+
+  test('Eventi.on("guard[\\]] escape", fn)', function() {
+    Eventi.on("guard[\\]] escape", function(e) {
+      equal(e.type, 'guard');
+    });
+    Eventi.fire("guard[]]");
+  });
+
   test('internal api presence', function() {
-    expect(0);
+    equal(typeof _.codes, "object", "_.codes");
+    equal(_.split.guard['['], ']');
   });
 
 }());
