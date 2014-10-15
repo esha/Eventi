3fb55134517a1e41b0ceead25411c3aabb558a9e
nbubna <nathan@esha.com>
2014-10-15 14:10:10 -0700

    don't duplicate location events and be more generous with key replacement

--- src/location.js
+++ src/location.js
@@ -43,7 +43,12 @@ if (global.history && global.location) {
                 }, uri);
             }
             if (uri !== current) {
-                history.pushState(null, null, encodeURI(uri));
+                if (e.location !== uri) {
+                    history.pushState(null, null, encodeURI(uri));
+                } else {
+                    _.pushState.call(history, null, null, encodeURI(uri));
+                    current = uri;
+                }
             }
         }
     };
@@ -60,7 +65,7 @@ if (global.history && global.location) {
         } else {
             re = re.replace(/([.*+?^=!:$(|\[\/\\])/g, "\\$1");// escape uri/regexp conflicts
             if (handler.keys = _.keys(re)) {
-                re = re.replace(/\{\w+\}/g, "([^\/?#]+)");
+                re = re.replace(/\{[\w@\-\.]+\}/g, "([^\/?#]+)");
             } else {
                 re.replace(/\{/g, '\\{');
             }
